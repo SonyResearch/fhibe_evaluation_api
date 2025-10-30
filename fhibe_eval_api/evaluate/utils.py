5dfd609a633f94a0a19ae667d340993c8de0cbcc
Austin Hoag <austin.hoag@sony.com>
2025-10-30 22:07:13 +0000

    Get evaluate task tests passing

--- fhibe_eval_api/evaluate/utils.py
+++ fhibe_eval_api/evaluate/utils.py
@@ -57,6 +57,7 @@ def get_eval_api(
     Return:
         The evaluation API class instance.
     """
+    print("processed_data_dir: ", processed_data_dir)
     annotations_csv_fp = os.path.join(
         processed_data_dir,
         dataset_name,
